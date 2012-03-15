0ab2e842d29b98e965126ba86c9810ba051ebd8c
Anastasia Cheetham <ac@anastasiacheetham.ca>
2012-03-15 14:07:49 -0400

    IDI-2: Simplify generation of top navigation.

--- topnav.php
+++ topnav.php
@@ -1,3 +1,3 @@
 			<nav role="navigation" class="fl-site-nav-main">
-				<?php wp_nav_menu( array( 'sort_column' => 'menu_order', 'theme_location' => 'primary-menu' ) ); ?>
+				<?php wp_nav_menu( array( 'depth' => '1' ) ); ?>
 			</nav>
